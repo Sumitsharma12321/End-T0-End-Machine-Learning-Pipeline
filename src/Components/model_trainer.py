c7dbf2ec5f3f08c2a7585b9d69e1ba2847d83a9b
Sumit Sharma <sumitsharma9098660389@gmail.com>
2026-07-05 23:51:18 +0530

    make app.py and templates

--- src/Components/model_trainer.py
+++ src/Components/model_trainer.py
@@ -97,6 +97,8 @@ class ModelTrainer:
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
+
+           
             best_model = models[best_model_name]
 
             if best_model_score<0.6:
